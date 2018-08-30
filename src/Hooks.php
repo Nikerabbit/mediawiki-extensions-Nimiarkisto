02ccc44b92b47fdf43aacb5aa413987b3ea30b15
Niklas Laxström <niklas.laxstrom@gmail.com>
2018-08-30 17:13:04 +0300

    Update maintenancepages,  add nimilippukuvat

--- src/Hooks.php
+++ src/Hooks.php
@@ -2,26 +2,16 @@
 
 namespace MediaWiki\Extension\Nimiarkisto;
 
+use RuntimeException;
+use SpecialPage;
+use Html;
+use Wikibase\Client\WikibaseClient;
+use Wikibase\DataModel\Entity\PropertyId;
+
 class Hooks {
 	public static function onBeforePageDisplay( $out, $skin ) {
 		$out->addModuleStyles( 'nimiarkistokartta.styles' );
 		$skin->getOutput()->addModules( 'nimiarkistokartta.init' );
-
-		$code = $out->getLanguage()->getCode();
-		switch ( $code ) {
-			case 'fi':
-			case 'se':
-			case 'smn':
-			case 'sms':
-			case 'sv':
-				$logoClass = "na-logo--$code";
-				break;
-			default:
-				$logoClass = 'na-logo--fi';
-		}
-
-		$out->addBodyClasses( 'na-logo' );
-		$out->addBodyClasses( $logoClass );
 
 		$userClass = $out->getUser()->isLoggedIn() ? 'na-user--user' : 'na-user--anon';
 		$out->addBodyClasses( $userClass );
@@ -42,12 +32,87 @@ class Hooks {
 HTML;
 			return [ $output, 'noparse' => true, 'isHTML' => true ];
 		} );
+
+		$parser->setFunctionHook( 'nimilippukuvat', function ( $parser, $param1 = '' ) {
+			$out = '';
+
+			try {
+				$images = self::getImages( $param1 );
+				$sp = SpecialPage::getTitleFor( 'FSIS' );
+				foreach ( $images as $x ) {
+					$out .= Html::element(
+						'img',
+						[ 'src' => $sp->getLocalUrl( [ 'g' => $x[ 'group' ], 'f' => $x[ 'file' ] ] ) ]
+					);
+				}
+			} catch ( RuntimeException $e ) {
+				$out = htmlspecialchars( "ERROR: {$e->getMessage()}" );
+			}
+
+			return [ $out, 'isHTML' => true ];
+		} );
 	}
+
+	private static function getImages( $input ) {
+		$wbc = WikibaseClient::getDefaultInstance();
+
+		// kl = keruulippu, nl = nimilippu
+		$klEntityId = $wbc->getEntityIdParser()->parse( $input );
+		$klEntity = $wbc->getStore()->getEntityLookup()->getEntity( $klEntityId );
+		$P10037 = new PropertyId( 'P10037' );
+		$producerStatements = $klEntity->getStatements()->getByPropertyId( $P10037 )->toArray();
+		if ( $producerStatements === [] ) {
+			return [];
+		}
+
+		$producer = $producerStatements[ 0 ]->getMainSnak()->getDataValue()->getEntityId()->getLocalPart();
+		// Q23 is Kotus
+		if ( $producer !== 'Q23' ) {
+			return [];
+		}
+		$group = 'Kotus';
+		$images = [];
+
+		$P10029 = new PropertyId( 'P10029' );
+		$klStatements = $klEntity->getStatements()->getByPropertyId( $P10029 );
+		foreach ( $klStatements as $klStatement ) {
+			$nlEntityId = $klStatement->getMainSnak()->getDataValue()->getEntityId();
+			$nlEntity = $wbc->getStore()->getEntityLookup()->getEntity( $nlEntityId );
+
+			// Whether image is hidden or not
+			$P10041 = new PropertyId( 'P10041' );
+			$jsStatements = $nlEntity->getStatements()->getByPropertyId( $P10041 );
+			foreach ( $jsStatements as $jsStatement ) {
+				$status = $jsStatement->getMainSnak()->getDataValue()->getEntityId()->getLocalPart();
+				if ( $status === 'Q28' ) {
+					continue 2;
+				}
+			}
+
+			$P10020 = new PropertyId( 'P10020' );
+			$nlStatements = $nlEntity->getStatements()->getByPropertyId( $P10020 );
+			foreach ( $nlStatements as $nlStatement ) {
+				$name = $nlStatement->getMainSnak()->getDataValue()->getValue();
+				$name = mb_strtoupper( $name );
+				$name = str_replace( 'KOTUS-', 'kotus-', $name );
+				$name = str_replace( '.JPG', '.jpg', $name );
+				$images[] = [
+					'group' => $group,
+					'file' => $name,
+				];
+			}
+		}
+
+		return $images;
+	}
+
 
 	public static function onLanguageGetMagic( &$raw ) {
 		// Convert formatted coordinates to plain coordinates
 		$raw['nac'] = [ 1, 'NAC' ];
 		// Allows moving the page title to different location
 		$raw['mytitle'] = [ 0, 'mytitle' ];
+
+		$raw['nimilippukuvat'] = [ 0, 'nimilippukuvat' ];
 	}
 }
