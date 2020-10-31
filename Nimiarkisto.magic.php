a0b9230644dd5159b07be1df889be60f9453efdc
Niklas Laxström <niklas.laxstrom@gmail.com>
2020-10-31 20:21:01 +0200

    Add missing files

---
+++ Nimiarkisto.magic.php
@@ -0,0 +1,9 @@
+<?php
+$magicWords = [];
+
+/** English (English) */
+$magicWords['en'] = [
+	'nac' => [ 1, 'NAC' ],
+	'mytitle' => [ 0, 'mytitle' ],
+	'nimilippukuvat' => [ 0, 'nimilippukuvat' ],
+];
