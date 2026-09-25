d04f0a4e36e0230030edb9a946749baf4e6d2ca5
Niklas Laxström <niklas.laxstrom@gmail.com>
2026-09-25 12:01:39 +0300

    More CI fixes

--- .phan/config.php
+++ .phan/config.php
@@ -9,7 +9,6 @@ $cfg['directory_list'] = array_merge(
 	[
 		'../SemanticMediaWiki',
 		'../Wikibase',
-		'../Wikibase/vendor/data-values/data-values/src',
 	]
 );
 
@@ -19,11 +18,7 @@ $cfg['exclude_analysis_directory_list'] = array_merge(
 		'vendor',
 		'../SemanticMediaWiki',
 		'../Wikibase',
-		'../Wikibase/vendor/data-values/data-values/src',
 	]
 );
-
-// Wikibase's other production dependencies overlap with MediaWiki core's vendor directory.
-$cfg['exclude_file_regex'] = '@^\.\./Wikibase/vendor/(?!data-values/)@';
 
 return $cfg;
