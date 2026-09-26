a7ad34c5b2b7681061c331dafcd0ddefae10afe1
Niklas Laxström <niklas.laxstrom@gmail.com>
2026-09-26 08:51:18 +0300

    More fixes

--- .phan/config.php
+++ .phan/config.php
@@ -7,6 +7,7 @@ $cfg['minimum_target_php_version'] = '8.3';
 $cfg['directory_list'] = array_merge(
 	$cfg['directory_list'],
 	[
+		'vendor/data-values/data-values/src',
 		'../SemanticMediaWiki',
 		'../Wikibase',
 	]
@@ -16,6 +17,7 @@ $cfg['exclude_analysis_directory_list'] = array_merge(
 	$cfg['exclude_analysis_directory_list'],
 	[
 		'vendor',
+		'vendor/data-values/data-values/src',
 		'../SemanticMediaWiki',
 		'../Wikibase',
 	]
